1d94b9637d98f569b29ee3872d006d5a835340f5
khaihq94 <hwk94@yahoo.com.vn>
2026-08-26 12:02:42 +0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        return set.size() != nums.length;
+    }
+}
